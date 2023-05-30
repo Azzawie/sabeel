2580014877ed2f75cddea5da26f16d3ce00d6d4c
Azzawie <mwmaki2@gmail.com>
2023-05-29 20:26:47 -0500

    adding cors gem to the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # protect_from_forgery with: :null_session
-  # skip_before_action :verify_authenticity_token
-
   add_flash_types :info, :error, :warn, :alert, :notice, :success
   before_action :set_locale
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
